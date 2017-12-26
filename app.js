c70e08d47f15d5d7d3818e0db71e815bd6bef17f
prodionov <pavelrodionov@hotmail.com>
2017-12-27 00:06:37 +0300

    add script to add a new user to the database and log the erorr if a username or an email are duplicates Reference #9

--- app.js
+++ app.js
@@ -1,19 +1,36 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = 4000;
+const port = 5000;
 const router = require("./src/controller/router");
+const su_router = require("./src/controller/signup");
+const morgan = require("morgan");
+const fs = require("fs");
+const bodyParser = require("body-parser");
 
 app.disable("x-powered-by");
+
+//the following two lines of code allow us to
+//access post data from the form
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//create a write stream in append mode
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "logs-demo", "access.log"),
+  { flags: "a" }
+);
+app.use(morgan("combined", { stream: accessLogStream }));
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(router);
+app.use('/signup_submit',su_router);
 
 //handling errors
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send("something broke");
 });
-
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(router);
 
 app.listen(port, () => {
   console.log(`I am listenning http://localhost:${port}`);
---
+++ src/controller/signup.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const su_router = express.Router();
+const add_user = require("../../db/query/add_user");
+
+su_router.post("/", async (req, res, next) => {
+  let data = req.body;
+  console.log("data", data);
+  try {
+    await add_user(data);
+  } catch (err) {
+    let error = err.detail;
+    if (
+      error.includes("username") &&
+      error.includes("already exists")
+    ) {
+      //make an injection into the signup.html that the user with this name already exist
+      console.log("we are here - username");
+      res.redirect("/signup");
+      next();
+    } else if(
+      error.includes("email") &&
+      error.includes("already exists")
+    ) {
+      console.log("we are here - email");
+      //make an injection into the signup.html that the user with this name already exist
+      res.redirect("/signup");
+      next();
+    } else {
+      res.redirect("/");
+      next();
+    }
+  }
+});
+
+module.exports = su_router;
